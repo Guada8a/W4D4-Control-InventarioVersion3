6fdc6d3922f6c24da1f90a4b29f2ad43ce5a8edf
Guadalupe Ochoa <jochoa10@ucol.mx>
2022-10-06 21:43:01 -0500

    reestructuracion en inventario.js, eliminación de eventos, corrección ordenarCodigo

--- modules/app.js
+++ modules/app.js
@@ -7,85 +7,44 @@ let btnAgregar = document.getElementById("btnAgregar");
 let btnAgregarPosicion = document.getElementById("btnAgregarPosicion");
 let btnBuscar = document.getElementById("btnBuscar");
 let btnEliminar = document.getElementById("btnEliminar");
-let btnListar = document.getElementById("btnListar");
-let btnListarInverso = document.getElementById("btnListarInverso");
 let operacion = document.getElementById("operacion");
-
 let codigo = document.getElementById("codigo");
 let nombre = document.getElementById("nombre");
 let costo = document.getElementById("costo");
 let cantidad = document.getElementById("cantidad");
-let mostrar1 = false, mostrar2 = false, mostrar3 = false;
 
-//OCULTAR SECCIONES
-document.getElementById("agregarh3").addEventListener("click", () => {
-    if (!mostrar1) {
-        document.getElementById("crud_agregar").style.display = "block";
-        mostrar1 = true;
-    } else {
-        document.getElementById("crud_agregar").style.display = "none";
-        mostrar1 = false;
-    } 
-});
-document.getElementById("buscarh3").addEventListener("click", () => {
-    if (!mostrar3) {
-        document.getElementById("crud_buscar").style.display = "block";
-        mostrar3 = true;
-    } else {
-        document.getElementById("crud_buscar").style.display = "none";
-        mostrar3 = false;
-    }
-});
-/***********/
+///EVENTOS
 btnAgregar.addEventListener("click", () => {
     let verifica = inventario.buscar(codigo.value);
-    if (verifica == null) {
+    if (verifica==false) {
         let producto = new Producto(codigo.value, nombre.value, cantidad.value, costo.value);
         inventario.agregar(producto);
+
+        inventario.ordenarCodigo();
+        let resListar = inventario.listado();
+        let resListarInverso = inventario.listadoInverso();
+
+        document.getElementById('listar').innerHTML = resListar;
+        document.getElementById('listarInverso').innerHTML = resListarInverso;
+        
         operacion.innerHTML += "Se agregó un elemento al inventario<hr>";
         operacion.scrollTop = operacion.scrollHeight;
-        codigo.value++;
+        codigo.style.border = "black solid 2px";
         nombre.value = "";
         costo.value = "";
         cantidad.value = "";
-    } else console.log("El código ya existe");
+    } else {
+        codigo.style.border = "red solid 2px";
+        console.log("El código ya existe");
+    }
     
-});
-btnAgregarPosicion.addEventListener("click", () => {
-    let posicion = document.getElementById("posicion").value;
-    let producto = new Producto(codigo.value, nombre.value, cantidad.value, costo.value);
-    inventario.agregarPosicion(producto, posicion);
-    operacion.innerHTML += `Se agregó un elemento al inventario en la posicion ${posicion}<hr>`;
-    operacion.scrollTop = operacion.scrollHeight;
-
-    codigo.value++;
-    nombre.value = "";
-    costo.value = "";
-    cantidad.value = "";
-});
-btnListar.addEventListener("click", () => {
-    let resListar = inventario.listado();
-    if (resListar != "") {
-        document.getElementById('listar').innerHTML = resListar;
-        operacion.innerHTML += "Se enlistaron los elementos del inventario<hr>";
-        operacion.scrollTop = operacion.scrollHeight;
-    }
-});
-btnListarInverso.addEventListener("click", () => {
-    let resListar = inventario.listadoInverso();
-    if (resListar != "") {
-        document.getElementById('listar').innerHTML = resListar;
-        operacion.innerHTML += "Se enlistaron los elementos del inventario en orden inverso<hr>";
-        operacion.scrollTop = operacion.scrollHeight;
-    }
 });
 btnBuscar.addEventListener("click", () => {
     let codigo = document.getElementById("buscar1").value;
     let divRes = document.getElementById("res");
-    //let elemento = inventario.buscar(codigo);
-    let elemento = inventario.busquedaBinaria(codigo);
+    let elemento = inventario.buscar(codigo);
     
-    if (elemento != null) {
+    if (elemento != false) {
         document.getElementById('title_search').style.display = "block";
         divRes.innerHTML = `<tr><td>Código</td><td>Nombre</td><td>Cantidad</td><td>Costo</td></tr><tr><td>${elemento.codigo} </td> <td>${elemento.nombre} </td><td> ${elemento.cantidad} </td><td>${elemento.costo}</td></tr>`;
         operacion.innerHTML += "Se buscó un elemento en el inventario<hr>";
@@ -112,11 +71,4 @@ btnEliminar.addEventListener("click", () => {
         divRes.innerHTML = "Ingrese un código válido";  
     }
 
-});
-btnOrdenar.addEventListener("click", () => {
-    let pre = inventario.ordenarCodigo();
-    if (pre != null) {
-        operacion.innerHTML += "Se ordenaron los elementos del inventario<hr>";
-        operacion.scrollTop = operacion.scrollHeight;
-    }
 });
--- modules/inventario.js
+++ modules/inventario.js
@@ -5,80 +5,7 @@ export default class Inventario{
     agregar(producto) {
         this.productos.push(producto);
     }
-    agregarPosicion(producto, posicion) {
-        let pIni = this.productos[posicion - 1];
-        if (posicion < this.productos.length+1) {
-            this.productos[posicion - 1] = producto;
-            for (let i = posicion + 1; i < this.productos.length; i++) {
-                let pTemp = this.productos[i];
-                this.productos[i] = pIni;
-                pIni = pTemp;
-            }
-        } else
-            console.log("No existe la posición");
-    }
-    eliminar(codigo) {
-        let product = false;
-        for (let i = 0; i <= this.productos.length; i++) {
-            if (this.productos[i])
-                if (codigo === this.productos[i].codigo) {
-                    for (let o = i; o < this.productos.length - 1; o++)
-                        this.productos[o] = this.productos[o + 1];
-
-                    this.productos[this.productos.length - 1] = 0;
-                    this.productos.pop();
-                    product = true;
-                }
-        }
-        return product;
-        
-    }
-    listado() {
-        let str = '<tr><td>Código</td><td>Nombre</td><td>Cantidad</td><td>Costo</td></tr >';
-        if (this.productos.length != 0){
-            for (let i = 0; i <= this.productos.length - 1; i++)
-                str += `<tr><td>${this.productos[i].codigo} </td> <td>${this.productos[i].nombre} </td><td> ${this.productos[i].cantidad} </td><td>${this.productos[i].costo}</td></tr>`;
-            return str;
-        } else {
-            console.log("No hay productos en el inventario");
-            return str = '';
-        }
-    }
-    ordenarCodigo() {
-        if (this.productos.length != 0) {
-            let i, j, aux;
-            for (i = 0; i < this.productos.length - 1; i++)
-                for (j = 0; j < this.productos.length - i - 1; j++)
-                    if (parseInt(this.productos[j + 1].codigo) < parseInt(this.productos[j].codigo)) {
-                        aux = this.productos[j + 1].codigo;
-                        this.productos[j + 1].codigo = this.productos[j].codigo;
-                        this.productos[j].codigo = aux;
-                    }
-            
-        } else {
-            console.log("No hay elementos en el inventario");
-            return null;
-        }
-    }
-    listadoInverso() {
-        let str = '<tr><td>Código</td><td>Nombre</td><td>Cantidad</td><td>Costo</td></tr >';
-        let aux = 0;
-        for (let k = 0; k < this.productos.length / 2; k++){
-            aux = this.productos[k];
-            this.productos[k] = this.productos[this.productos.length - 1 - k];
-            this.productos[this.productos.length - 1 - k] = aux;
-        }
-        if (this.productos.length != 0) {
-            for (let i = 0; i <= this.productos.length-1; i++){
-                str += `<tr><td>${this.productos[i].codigo} </td> <td>${this.productos[i].nombre} </td><td> ${this.productos[i].cantidad} </td><td>${this.productos[i].costo}</td></tr>`;
-            }
-            return str;
-        } else {
-            console.log("No hay productos en el inventario");
-            return str = "";
-        }
-    }
-    busquedaBinaria(codigo) {
+    buscar(codigo) {
         let producto = false;
         let inicio = 0;
         let fin = this.productos.length - 1;
@@ -95,10 +22,65 @@ export default class Inventario{
         }
         return producto;
     }
-    buscar(codigo) {
-        for (let i = 0; i <= this.productos.length; i++)
+    eliminar(codigo) {
+        let product = false;
+        for (let i = 0; i <= this.productos.length; i++) {
             if (this.productos[i])
-                if (codigo === this.productos[i].codigo)
-                    return this.productos[i];
+                if (codigo === this.productos[i].codigo) {
+                    for (let o = i; o < this.productos.length - 1; o++)
+                        this.productos[o] = this.productos[o + 1];
+
+                    this.productos[this.productos.length - 1] = 0;
+                    this.productos.pop();
+                    product = true;
+                }
+        }
+        return product;
     }
+    listado() {
+        let str = '<tr><td>Código</td><td>Nombre</td><td>Cantidad</td><td>Costo</td><td>Total</td></tr >';
+        if (this.productos.length != 0){
+            for (let i = 0; i <= this.productos.length - 1; i++)
+                str += `<tr><td>${this.productos[i].codigo} </td> <td>${this.productos[i].nombre} </td><td> ${this.productos[i].cantidad} </td><td>${this.productos[i].costo}</td><td>${this.productos[i].total}</td></tr>`;
+            return str;
+        } else {
+            console.log("No hay productos en el inventario");
+            return str = '';
+        }
+    }
+    listadoInverso() {
+        let str = '<tr><td>Código</td><td>Nombre</td><td>Cantidad</td><td>Costo</td><td>Total</td></tr >';
+        let aux = 0;
+        for (let k = 0; k < this.productos.length / 2; k++){
+            aux = this.productos[k];
+            this.productos[k] = this.productos[this.productos.length - 1 - k];
+            this.productos[this.productos.length - 1 - k] = aux;
+        }
+        if (this.productos.length != 0) {
+            for (let i = 0; i <= this.productos.length-1; i++){
+                str += `<tr><td>${this.productos[i].codigo} </td> <td>${this.productos[i].nombre} </td><td> ${this.productos[i].cantidad} </td><td>${this.productos[i].costo}</td><td>${this.productos[i].total}</td></tr>`;
+            }
+            return str;
+        } else {
+            console.log("No hay productos en el inventario");
+            return str = "";
+        }
+    }
+    ordenarCodigo() {
+        if (this.productos.length != 0) {
+            let i, j, aux;
+            for (i = 0; i < this.productos.length - 1; i++)
+                for (j = 0; j < this.productos.length - i - 1; j++)
+                    if (parseInt(this.productos[j + 1].codigo) < parseInt(this.productos[j].codigo)) {
+                        aux = this.productos[j + 1];
+                        this.productos[j + 1] = this.productos[j];
+                        this.productos[j] = aux;
+                    }
+            
+        } else {
+            console.log("No hay elementos en el inventario");
+            return null;
+        }
+    }
+    
 }
